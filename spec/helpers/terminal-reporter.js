b6c95c17ca338dee4a4333172ce52e1685d77506
Lukas <Bone008@users.noreply.github.com>
2020-11-16 18:10:55 +0100

    Add jasmine-console-reporter for better test log

---
+++ spec/helpers/terminal-reporter.js
@@ -0,0 +1,5 @@
+// See: https://github.com/onury/jasmine-console-reporter
+var Reporter = require("jasmine-console-reporter");
+const reporter = new Reporter({ isVerbose: true });
+
+jasmine.getEnv().addReporter(reporter);
